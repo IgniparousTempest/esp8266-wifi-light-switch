71429f76e78726bfb155a0b6e31cdfcbe9bd40fa
Courtney Pitcher <cr.pitcher@gmail.com>
2017-11-28 02:32:31 +0200

    Improved restful compliance

--- WiFi_Light.ino
+++ WiFi_Light.ino
@@ -3,6 +3,7 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 //------------------------------for local web
+#include <ArduinoJson.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
@@ -15,13 +16,13 @@ const char* ssid = "ssid";
 const char* password = "password";
 int pinout=2;  //for relay, and internal led
 int pinsw=0;  //for switch
-boolean light_change;
+boolean light_status;
 boolean sw_status;
 
 void setup() {
   pinMode(pinout, OUTPUT);
   pinMode(pinsw, INPUT);
-  Serial.begin(74880);
+  Serial.begin(115200);
   Serial.println("Booting");
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -57,8 +58,8 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   //-----------------------------------------add your code here
-  webPage += "<center><font size='27'>ESP8266 Web Server<p>Light #1 ";
-  webPage += "<a href=\"toggle_light\"><mark>TOGGLE it";
+  webPage += "<center><font size='27'>ESP8266 Web Server Light Switch<p>Light #1 ";
+  webPage += "<a href=\"/light/toggle\"><mark>TOGGLE it";
   webPage += "</mark></a></p></font></center>";
 
   if (mdns.begin("esp8266", WiFi.localIP())) 
@@ -67,33 +68,61 @@ void setup() {
   server.on("/", [](){
     server.send(200, "text/html", webPage);
   });
-  server.on("/toggle_light", [](){
+  server.on("/light/toggle", [](){
+    light_toggle();
     server.send(200, "text/html", webPage);
-    // Change status
-    light_change = true;
-    delay(100);
+  });
+  server.on("/light", HTTP_GET, [](){
+    server.send(200, "text/json", response_json());
+  });
+  server.on("/light", HTTP_POST, [](){
+    StaticJsonBuffer<200> request_buffer;
+    JsonObject& request = request_buffer.parseObject(server.arg("plain"));
+    boolean desired_light_state = request["active"];
+    if (desired_light_state)
+      light_on();
+    else
+      light_off();
+    server.send(200, "text/json", response_json());
   });
 
   server.begin();
   //-------------------------------------------add your code here
 }
 
+String response_json() {
+    String is_active = light_status ? "true" : "false";
+    return "{\"is_active\": \"" + is_active + "\"}";
+}
+
+void light_toggle() {
+    Serial.println("Toggling light...");
+    if (light_status) 
+      light_off();
+    else
+      light_on();
+}
+
+void light_off() {
+    Serial.println("Turning light off");
+    light_status = false;
+    digitalWrite(pinout, LOW);
+}
+
+void light_on() {
+    Serial.println("Turning light on");
+    light_status = true;
+    digitalWrite(pinout, HIGH);
+}
 
 void loop() {
   ArduinoOTA.handle();
   //-------------------------------------------add your code here
   server.handleClient();
-  if(light_change)
-  {
-    light_change = false;
-    if(digitalRead(pinout)) digitalWrite(pinout,LOW);
-    else digitalWrite(pinout,HIGH);
-  }
 
   if(sw_status != digitalRead(pinsw))
   {
     sw_status = digitalRead(pinsw);
-    light_change = true;
   }
   delay(200);
   
